bd93367feee593f7d39b7122d3e68f3f0e35ffe9
Doan Van Binh <vanbinhdoan971@gmail.com>
2022-06-01 15:06:28 +0700

    fix some logic

--- src/main/java/com/example/jwtandwebsocket/controller/WebsocketController.java
+++ src/main/java/com/example/jwtandwebsocket/controller/WebsocketController.java
@@ -1,8 +1,10 @@
 package com.example.jwtandwebsocket.controller;
 
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 
+import java.security.Principal;
 import java.time.LocalDate;
 
 @Controller
@@ -11,7 +13,8 @@ public class WebsocketController extends BaseWebsocketController {
     // handler all send request from client to path "/app/greeting",
     // the returned value being sent to broker channel, then being sent to topic "/topic/greeting"
     @MessageMapping("/greeting")
-    public String sendGreetingMessage(String message) {
+    public String sendGreetingMessage(String message,  Principal user,
+                                      @Header("simpSessionId") String sessionId) {
         System.out.println("Getting some message: " + message);
         return "[" + LocalDate.now().toString() + ": " + message;
     }
